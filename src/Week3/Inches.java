9c0278080843baa5fce749fd64224844a22b5a61
thomas.jordan@my.jcu.edu.au <Yakity123>
2019-08-21 14:25:46 +1000

    Added class Inches and test class inchesTest

---
+++ src/Week3/Inches.java
@@ -0,0 +1,15 @@
+package Week3;
+
+public class Inches {
+
+    static final int INCHES = 100;
+    static double feet = INCHES * 0.08333;
+
+    static void printValues() {
+        System.out.println("Inches: " + INCHES);
+        System.out.println("Feet: " + feet);
+    }
+}
+
+
+
---
+++ src/Week3/InchesTest.java
@@ -0,0 +1,15 @@
+package Week3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InchesTest {
+    @Test
+    void testInches(){
+
+        Inches inches = new Inches();
+        Inches.printValues();
+
+    }
+}

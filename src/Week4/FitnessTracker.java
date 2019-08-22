62c1f33b53a4584c991b075ef7c89ec58e3ae6bd
tom <tomjordan505@gmail.com>
2019-08-22 22:46:55 +1000

    Added fitnesstracker class and fitness tracker test to finish off prac 3

---
+++ src/Week4/FitnessTracker.java
@@ -0,0 +1,42 @@
+package Week4;
+
+public class FitnessTracker {
+
+    private int minutes;
+    private int year = 2019;
+    private int day;
+    private String month;
+
+   public FitnessTracker(){
+        this.day = 1;
+        this.month = "January";
+        this.minutes = 0;
+    }
+
+    public FitnessTracker(int day, String month, int year, int minutes){
+        this.day = day;
+        this.month = month;
+        this.minutes = minutes;
+        this.year = year;
+    }
+
+
+
+
+    int getMinutes(){
+       return this.minutes;
+    }
+
+    int getYear(){
+       return this.year;
+    }
+
+    int getDay(){
+       return this.day;
+    }
+
+    String getMonth(){
+       return this.month;
+    }
+
+}

64f74e7ac14daffab76102c00378b2f1d42bee4e
thomas.jordan@my.jcu.edu.au <Yakity123>
2019-08-22 15:18:22 +1000

    Changed inches interactive to call methods rather than have all code in main

--- src/Week3/InchesToFeetInteractive.java
+++ src/Week3/InchesToFeetInteractive.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 import java.io.Console;
 
 public class InchesToFeetInteractive {
-
     public static void main(String args[]) {
 
 
         Scanner myInput = new Scanner(System.in);
         System.out.print("Enter inches: ");
-        int a;
-        a = myInput.nextInt();
+        int a = myInput.nextInt();
+
+        printValues(a);
+
+        System.out.print("Enter inches: ");
+        int b = myInput.nextInt();
+        printValues(b);
+
+    }
+
+    static void printValues(int a) {
         double feet = a * 0.08333;
         double feet_out = Math.floor(feet);
         double inches_out = a - feet_out * 12;
@@ -19,24 +27,8 @@ public class InchesToFeetInteractive {
         System.out.println("Inches: " + a);
         System.out.println("Feet: " + feet_out + " Inches: " + inches_out);
 
-
     }
 }
-
-
-
-/*
-
-    void printValues(int a) {
-        double feet = a * 0.08333;
-        double feet_out = Math.floor(feet);
-        double inches_out = a - feet_out * 12;
-
-        System.out.println("Inches: " + a);
-        System.out.println("Feet: " + feet_out + " Inches: " + inches_out);
-    }
-}
-*/
 
 
 
---
+++ src/Week4/Sandwich.java
@@ -0,0 +1,15 @@
+package Week4;
+
+public class Sandwich {
+
+    String mainingrediant;
+    String breadtype;
+    Double price;
+
+
+        public static void main(String args[]) {
+
+        }
+
+
+}

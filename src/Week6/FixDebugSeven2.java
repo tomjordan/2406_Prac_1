c4674dc55d4336d5b92710c7412ca79c9fade83b
tom <thomas.jordan@my.jcu.edu.au>
2019-09-03 13:12:27 +1000

    finished debug seven exercises

---
+++ src/Week6/FixDebugSeven2.java
@@ -0,0 +1,35 @@
+package Week6;
+
+import java.util.Scanner;
+
+public class FixDebugSeven2 {
+    public static void main(String[] args) {
+        String str;
+        int x;
+        int length;
+        int start = 0;
+        int num;
+        int lastSpace = -1;
+        int sum = 0;
+        String partStr;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a series of integers separated by spaces >> ");
+        str = in.nextLine();
+        length = str.length();
+        for (x = 0; x < length; ++x) {
+            if (str.charAt(x) == ' ') {
+                partStr = str.substring(lastSpace + 1, x);
+                num = Integer.parseInt(partStr);
+                System.out.println("                " + num);
+                sum += num;
+                lastSpace = x;
+            }
+        }
+        partStr = str.substring(lastSpace + 1, length);
+        num = Integer.parseInt(partStr);
+        System.out.println("                " + num);
+        sum += num;
+        System.out.println("         -------------------" +
+                "\nThe sum of the integers is " + sum);
+    }
+}
